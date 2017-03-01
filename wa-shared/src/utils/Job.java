53dbb09dac7c82a3eb57d24fd59b1763339375f7
naraekim12 <naraekim12@hotmail.com>
2017-03-01 13:44:49 +0000

    base code

--- wa-shared/src/utils/Job.java
+++ wa-shared/src/utils/Job.java
@@ -3,12 +3,12 @@ package utils;
 public class Job {
 
   int jobID;
-	ArrayList<Task> task= new ArrayList<Task>();
+  ArrayList<Task> task= new ArrayList<Task>();
 	
-	public int sumOfWeight(int quantity, int weight){
-		
-		int sumOfWeight = quantity*weight;
-		return sumOfWeight;
+  public int sumOfWeight(int quantity, int weight){
+
+	int sumOfWeight = quantity*weight;
+	return sumOfWeight;
   }
   
 }
