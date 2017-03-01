399933d38cd8e9bd8f0c196fb654d64f03ea976c
jxv603 <jxv603@cs.bham.ac.uk>
2017-03-01 14:57:51 +0000

    start import from csv

--- wa-pc/src/job/Input.java
+++ wa-pc/src/job/Input.java
@@ -1,5 +1,38 @@
 package job;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import utils.Job;
+
 public class Input {
+
+	private ArrayList<Job> jobs= new ArrayList<Job>();
+	
+	public static void createListOfJobs() {
+
+		String csvFile = "name.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+				String[] parts = line.split(cvsSplitBy);
+				
+				
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
