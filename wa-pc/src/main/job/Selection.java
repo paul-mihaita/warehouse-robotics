d82bc9a07b000645beaf969db077133be0220a0a
bxk631 <bxk631@cs.bham.ac.uk>
2017-03-06 16:07:14 +0000

    implementation of Assignment and Selection classes for Jobs

--- wa-pc/src/main/job/Selection.java
+++ wa-pc/src/main/job/Selection.java
@@ -1,5 +1,41 @@
 package main.job;
 
-public class Selection {
+import java.util.ArrayList;
 
+import utils.Job;
+import utils.Location;
+import utils.Robot;
+
+public class Selection extends Thread{
+
+	private Robot robot;
+	private boolean run;
+	private ArrayList<Job> copyJobs;
+	private Location startLocation;
+	
+	public Selection(Robot robot, ArrayList<Job> jobs){
+		
+		this.robot=robot;
+		this.startLocation=this.robot.getCurrentLocation();
+		this.copyJobs=new ArrayList<Job>();
+		
+		for(Job j:jobs){
+			this.copyJobs.add(j);
+		}
+		
+		this.start();
+	}
+	
+	public void run(){
+		
+		this.run=true;
+		
+		//JobWorth bestJob;
+		while(run){
+			if(this.copyJobs.size()<=0){
+				
+				break;
+			}
+		}
+	}
 }
