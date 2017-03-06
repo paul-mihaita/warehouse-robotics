8fdae6bd03f3a3a7cff429a31c409a09ce655b72
Lexer747 <Lexer747@gmail.com>
2017-03-06 17:05:26 +0000

    added log file for the bootStraper

--- wa-pc/src/bootstrap/Start.java
+++ wa-pc/src/bootstrap/Start.java
@@ -1,12 +1,15 @@
 package bootstrap;
 
+import org.apache.log4j.Logger;
+
 import main.job.Input;
 import main.pc_gui.GUI;
 
 public class Start {
+	private static Logger log = Logger.getLogger(Start.class);
 
 	public static void main(String[] args) {
-		
+		log.info("Starting");
 		Input in = new Input(false);
 		in.initializeListOfJobs("1", "2", "3");
 		GUI.create(in.getJobsArray());
