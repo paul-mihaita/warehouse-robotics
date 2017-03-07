56a7db74d55c2fc9b1856adc482933889df7a0a5
paulmihaita <pxm673@cs.bham.ac.uk>
2017-03-07 16:09:15 +0000

    addded commandcenter

--- wa-pc/src/route/test.java
+++ wa-pc/src/route/test.java
@@ -23,32 +23,38 @@ public class test {
 		GridMap gridMap = MapUtils.createRealWarehouse();
 		Graph<Location> graph = Planning.createGraph(gridMap);
 		Location start = new Location(0,1);
-		Location finish = new Location(0,1);
+		Location finish = new Location(0,7);
 		Location orientation = new Location(0,3);
 		
-		BiFunction<Location, Location, Integer> heuristics = new BiFunction<Location, Location, Integer> (){
+		/*BiFunction<State, State, Integer> manhatan = new BiFunction<State, State, Integer> (){
 
 			@Override
-			public Integer apply(Location t, Location u) {
+			public Integer apply(State a, State b) {
 				// TODO Auto-generated method stub
-				return (Math.abs(t.getX() - u.getX()) + Math.abs(t.getY() - u.getY()));
+				List<Location> from = a.getRLoc();
+				List<Location> to = b.getRLoc();
+				int cost = 0;
+				for(int i = 0; i < from.size();i++){
+					Location t = from.get(i);
+					Location u = to.get(i);
+					cost+= (Math.abs(t.getX() - u.getX()) + Math.abs(t.getY() - u.getY()));
+				}
+				return cost;
 			}
 			
 		};
-		/*
-		ArrayList<IVertex<Location>> route = plan.aStarSimple(graph,start ,finish ,  heuristics);
-		for(IVertex<Location> n : route){
-			Location l = (Location) n.getLabel().getData();
+		ArrayList<Location> route = Astar.aStar(graph,start ,finish , manhatan, 3 ,false,gridMap);
+		for(Location l : route){
 			System.out.print(l.getX() + "  " + l.getY() + " | ");
 		}
 		System.out.println();
-		System.out.println(Planning.simpleAc);
+		System.out.println(Astar.simpleAc);
 */
 		State s = new State();
 		ArrayList<Location> l = new ArrayList<>();
 		Location a = new Location(3,0);
 		l.add(a);
-		Location b = new Location(4,0);
+		Location b = new Location(0,0);
 		l.add(b);
 		Location c = new Location(5,0);
 		l.add(c);
@@ -58,10 +64,8 @@ public class test {
 		Vertex<Location> p = (Vertex<Location>) graph.getVertex(c);
 		ArrayList<Location> fin = new ArrayList<>();
 		fin.add(new Location(0,0));
-		fin.add(new Location(-1,-1));
+		fin.add(new Location(3,0));
 		fin.add(new Location(10,0));
-		//fin.add(new Location(-1,-1));
-		//fin.add(new Location(-1,-1));
 		State t = new State();
 		t.setRLoc(fin);
 		
@@ -73,69 +77,8 @@ public class test {
 		robots.add(r2);
 		robots.add(r3);
 		HashMap<Location,HashMap<Location,Integer>> costs = new HashMap<>();
-		BiFunction<State, State, Integer> heuristics2 = new BiFunction<State, State, Integer>() {
-
-			@Override
-			public Integer apply(State t, State u) {
-				int cost = 0;
-				List<Location> robots = t.getRLoc();
-				List<Location> finish = u.getRLoc();
-				for(int i=0;i<robots.size();i++){
-					Location a = robots.get(i);
-					Location b = finish.get(i);
-					if(costs.containsKey(a)){
-						HashMap<Location,Integer> x = costs.get(a);
-						if(x.containsKey(b)){
-							cost+= x.get(b);
-						}else{
-							Astar.aStar(graph, a, b, heuristics);
-							x.put(b,Astar.simpleAc.intValue());
-							cost+= Astar.simpleAc;
-						}
-					}
-					else{
-						Astar.aStar(graph, a, b, heuristics);
-						HashMap<Location, Integer> x = new HashMap<>();
-						x.put(b,Astar.simpleAc.intValue());
-						costs.put(a, x);
-						cost+= Astar.simpleAc;
-					}
-				}
-				return cost;
-			}
-			
-		};
-		HashMap <Integer,ArrayList<Location> >mapp = new HashMap<>();
-		ArrayList<State> pp = AstarBruteForce.aStar(graph, gridMap, robots, fin, heuristics2, 100);
-		boolean ok = true;
-		for(State sta : pp){
-			List<Location> lll = sta.getRLoc();
-
-			for (int i = 0; i < lll.size(); i++) {
-				Location pl = lll.get(i);
-				if(ok == true){
-					ArrayList<Location> kl = new ArrayList<Location>();
-					kl.add(pl);
-					mapp.put(i, kl);
-				}else{
-					mapp.get(i).add(pl);
-				}
-			}
-			ok = false;
-		}
-		Iterator<ArrayList<Location>> it = mapp.values().iterator();
-		int i = 0;
-		while(it.hasNext()){
-			ArrayList<Location> ghf = it.next();
-			Collections.reverse(ghf);
-			System.out.print("robot "+ i + ":  ");
-			i++;
-			for(Location lo : ghf){
-				System.out.print( lo.getX() + " "+lo.getY() + " | ");
-			}
-			System.out.println();
-		}
-		System.out.println(AstarBruteForce.simpleAc);
 		
+		HashMap <Integer,ArrayList<Location> >mapp = new HashMap<>();
+		AstarHCA.aStar(graph, gridMap, robots, fin, 100);
 	}
 }
