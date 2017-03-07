c773aa611d5219a58ecd1131e769369f4a3bab91
Lexer747 <Lexer747@gmail.com>
2017-03-07 13:09:25 +0000

    added sample code for a robot object

--- wa-pc/src/bootstrap/Start.java
+++ wa-pc/src/bootstrap/Start.java
@@ -4,11 +4,14 @@ import org.apache.log4j.Logger;
 
 import main.job.Input;
 import main.pc_gui.GUI;
+import utils.Location;
+import utils.Robot;
 
 public class Start {
 
 	private static Logger log = Logger.getLogger(Start.class);
 
+	private static Robot r = new Robot("Keith", "0016530FDDAE", new Location(0, 1), new Location(0, 0));
 	public static void main(String[] args) {
 		log.info("Starting");
 		Input in = new Input(false);
