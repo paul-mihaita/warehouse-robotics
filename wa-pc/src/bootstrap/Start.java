4a0e38ec44dca1f725d5e04a35413136685f7df5
Tom Taylor <txt452@student.bham.ac.uk>
2017-03-08 13:12:38 +0000

    Integrated path finding with GUI start function

--- wa-pc/src/bootstrap/Start.java
+++ wa-pc/src/bootstrap/Start.java
@@ -2,13 +2,11 @@ package bootstrap;
 
 import org.apache.log4j.Logger;
 
-import communication.thread.Server;
 import main.gui.GUI;
 import main.job.Input;
 import main.model.WarehouseFloor;
 import main.route.Planning;
 import rp.robotics.mapping.MapUtils;
-import utils.Robot;
 
 public class Start {
 
@@ -17,10 +15,13 @@ public class Start {
 	public static void main(String[] args) {
 
 		log.info("Starting");
+
 		Input in = new Input(false);
 		in.initializeListOfJobs("1", "2", "3");
+
 		WarehouseFloor model = new WarehouseFloor(Planning.createGraph(MapUtils.createRealWarehouse()),
 				in.getJobsArray(), log);
+
 		GUI.create(model);
 	}
 
