5ba118cf74d38eaf3e1dffcf1210aa1c1b724d79
Lexer747 <Lexer747@gmail.com>
2017-03-14 14:26:10 +0000

    fixed minor things to get it to run

--- wa-pc/src/main/model/WarehouseFloor.java
+++ wa-pc/src/main/model/WarehouseFloor.java
@@ -33,7 +33,7 @@ public class WarehouseFloor {
 	private Graph<Location> floor;
 
 	private Logger log;
-	
+
 	private boolean server;
 
 	/**
@@ -55,13 +55,15 @@ public class WarehouseFloor {
 		this.jobList = new HashMap<Integer, Job>();
 		this.robots = new HashSet<Robot>();
 		this.messageQueues = new HashMap<String, Message>();
-		
-		//Robot keith = new Robot("Keith", "0016530FDDAE", new Location(2, 0), new Location(1,0));
+
+		// Robot keith = new Robot("Keith", "0016530FDDAE", new Location(2, 0),
+		// new Location(1,0));
 		Robot cell = new Robot("Cell", "0016531AFA0B", new Location(0, 1), new Location(1, 0));
-		//Robot charmander = new Robot("Charmander", "0016531AF6D6", new Location(0, 1), new Location(2, 0));
-		//this.robots.add(keith);
+		// Robot charmander = new Robot("Charmander", "0016531AF6D6", new
+		// Location(0, 1), new Location(2, 0));
+		// this.robots.add(keith);
 		this.robots.add(cell);
-		//this.robots.add(charmander);
+		// this.robots.add(charmander);
 
 		for (Job j : jobs) {
 			jobList.put(j.getJobID(), j);
@@ -85,24 +87,23 @@ public class WarehouseFloor {
 				maxkey = key;
 			}
 		}
-		
-		
-		//this.assign("Cell", jobList.get(maxkey));
-		
+
+		// this.assign("Cell", jobList.get(maxkey));
+
 		temp.remove(maxkey);
-		
+
 		maxkey = -1;
-		
-		for (Integer key: temp.keySet()){
-			if(maxkey == -1){
+
+		for (Integer key : temp.keySet()) {
+			if (maxkey == -1) {
 				maxkey = key;
 			}
-			if(temp.get(key) > temp.get(maxkey)){
+			if (temp.get(key) > temp.get(maxkey)) {
 				maxkey = key;
 			}
 		}
 
-		//this.assign("Keith", jobList.get(maxkey));
+		// this.assign("Keith", jobList.get(maxkey));
 
 		// temp.remove(maxkey);
 		//
@@ -122,8 +123,9 @@ public class WarehouseFloor {
 
 		this.floor = floor;
 		log.debug("Creating Server");
-		Server s = new Server(new Robot[] { cell }  , tempArr, log);
-		if (server) s.launch();
+		Server s = new Server(new Robot[] { cell }, tempArr, log);
+		if (server)
+			s.launch();
 		log.debug("Server launched succesfully, warehousefloor constructed");
 	}
 
@@ -164,7 +166,8 @@ public class WarehouseFloor {
 	}
 
 	private void givePaths(HashMap<Robot, ArrayList<ArrayList<move>>> routes) {
-		if (!server) return;
+		if (!server)
+			return;
 		RobotHelper[] help = new RobotHelper[robots.size()];
 		int i = 0;
 		for (Robot robot : robots) {
