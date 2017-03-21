bc074532291d44ebd2fc0e094888f5357060e28e
Tom Taylor <txt452@student.bham.ac.uk>
2017-03-21 17:55:27 +0000

    Ugh, got to add a final location

--- wa-shared/src/utils/DropLocation.java
+++ wa-shared/src/utils/DropLocation.java
@@ -4,25 +4,42 @@ public class DropLocation {
 
 	private String name;
 	private Location location;
-	
-	public DropLocation(String name){
-		this.name=name;
+	private boolean taken;
+
+	public DropLocation(String name) {
+		this.name = name;
 		this.location = new Location(0, 0);
+		this.taken = false;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Location getLocation() {
 		return location;
 	}
+
 	public void setLocation(int x, int y) {
 		this.location.setX(x);
 		this.location.setY(y);
 	}
-	
-	
+
+	public void reserved(boolean b) {
+		this.taken = b;
+	}
+
+	public boolean isReserved() {
+		return taken;
+	}
+
+	@Override
+	public String toString() {
+		return "Name: " + name + " Location: " + location.toString();
+	}
+
 }
