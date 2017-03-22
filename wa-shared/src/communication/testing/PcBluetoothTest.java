28bdde2de91ec854c20012a730da4e840e1747cf
Lexer747 <Lexer747@gmail.com>
2017-03-22 18:52:33 +0000

    finished junit test

--- wa-shared/src/communication/testing/PcBluetoothTest.java
+++ wa-shared/src/communication/testing/PcBluetoothTest.java
@@ -31,8 +31,11 @@ public class PcBluetoothTest {
 	@Before
 	public void setUp() {
 		for (int i = 0; i < msgs.length; i++) {
-			BasicJob job = new BasicJob(1, new Task("a", 0));
-			msgs[i] = new Message(new ArrayList<move>(), command.Wait, job);
+			BasicJob job = new BasicJob(1, new Task("a", 1));
+			ArrayList<move> moves = new ArrayList<move>();
+			moves.add(move.BACKWARD);
+			msgs[i] = new Message(moves, command.Wait, job);
+			log.info(msgs[i].toString());
 		}
 		Server s = new Server(robots, msgs, log);
 		s.launch(); // wait for the robots to connect
@@ -40,18 +43,26 @@ public class PcBluetoothTest {
 
 	@Test
 	public void test() {
+		
+		log.info("running test");
 		Robot[] previous = new Robot[robots.length];
 		for (int i = 0; i < previous.length; i++) {
 			previous[i] = robots[i].cloneRobot();
 		}
-		Delay.msDelay(1000); //wait for original data to send
+		log.info("sent robots");
+		Delay.msDelay(1000); // wait for original data to send
+		log.info("sending command");
 		for (int i = 0; i < msgs.length; i++) {
-			msgs[i].setCommand(command.Finish); 
-			//send a bluetooth command
-			//the robots array will be changed by robots
+			msgs[i].setCommand(command.Finish);
+			Delay.msDelay(1000);
+			msgs[i].setCommand(command.Finish);
+			log.info(msgs[i].toString());
+			// send a bluetooth command
+			// the robots array will be changed by robots
 		}
-		//wait for the response
+		// wait for the response
 		Delay.msDelay(5000);
+		log.info("testing recieved object");
 		for (int i = 0; i < previous.length; i++) {
 			assertTrue(!compareRobot(previous[i], robots[i]));
 		}
